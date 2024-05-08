40c9c42540eea7d88f498ebae4793d961eaa921d
Donggeon Kim <ddss217@gmail.com>
2024-05-08 20:46:43 +0900

    sum함수를 사용하지 않게 바꿈

--- passing_truck.py
+++ passing_truck.py
@@ -1,11 +1,14 @@
 def solution(bridge_length, weight, truck_weights):
     answer = 0
     bridge = [0] * bridge_length
+    weight_now = 0
     while bridge:
-        bridge.pop(0)
+        weight_now -= bridge.pop(0)
         if truck_weights:
-            if sum(bridge) + truck_weights[0] <= weight:
-                bridge.append(truck_weights.pop(0))
+            if weight_now + truck_weights[0] <= weight:
+                truck = truck_weights.pop(0)
+                bridge.append(truck)
+                weight_now += truck
             else:
                 bridge.append(0)
         answer += 1
