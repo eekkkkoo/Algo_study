4363fa7a97176466e5a446c652c75370a9c4dd21
Donggeon Kim <ddss217@gmail.com>
2024-05-18 20:11:12 +0900

    백준 2178번 미로 탐색

---
+++ baekjoon/2178.py
@@ -0,0 +1,22 @@
+# 입력
+N, M = map(int, input().split())
+graph = [0] * N
+for i in range(N):
+    graph[i] = list(map(int, input()))
+dx = [0, 1, 0, -1]
+dy = [1, 0, -1, 0]
+# DFS함수
+def BFS(graph, x, y):
+    queue = [(x, y)]
+    while queue:
+        x, y = queue.pop(0)
+        for i in range(4):
+            X = x + dx[i]
+            Y = y + dy[i]
+            if 0 <= X < N and 0<= Y < M:
+                if graph[X][Y] == 1:
+                    graph[X][Y] = graph[x][y] + 1
+                    queue.append((X, Y))
+
+BFS(graph, 0, 0)
+print(graph[N-1][M-1])
