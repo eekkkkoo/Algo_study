c4dceade397a72798e48ce00ea35ea8c731234a9
Donggeon Kim <ddss217@gmail.com>
2024-05-24 12:05:16 +0900

    백준 2875번 대회 or 인턴

---
+++ baekjoon/2875.py
@@ -0,0 +1,12 @@
+N, M, K = map(int, input().split())
+
+while K:
+    if N > M:
+        if N > 2*M:
+            N -= 1
+        else:
+            M -= 1
+    else:
+        M -= 1
+    K -= 1
+print(min(N//2, M))
